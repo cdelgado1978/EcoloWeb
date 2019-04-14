30f33792e2abed11a6f29d965b567f92e105a011
Yordin-Checo <39702531+Yordin-Checo@users.noreply.github.com>
2019-04-14 11:48:34 -0400

    Cambios Yordin

--- EcoloWeb/Controllers/HomeController.cs
+++ EcoloWeb/Controllers/HomeController.cs
@@ -5,12 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EcoloWeb.Models;
+using EcoloWeb.Data;
 using Microsoft.AspNetCore.Authorization;
+using EcoloWeb.Data.Entity;
+using EcoloWeb.Data.Entity.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcoloWeb.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -51,5 +63,44 @@ namespace EcoloWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        ///Esto tiene que ver con la parte del Comprimiso mientras se ubica 
+
+        public async Task<IActionResult> Compromiso()
+        {
+
+
+            //var Tienda = from m in _context.Compromisos.Include(a => a.Notas) select m;
+
+            //if (!String.IsNullOrEmpty(Buscar))
+            //{
+            //    Tienda = Tienda.Where(s => s.Producto.Contains(Buscar));
+            //}
+            return View(await _context.Compromisos.ToListAsync());
+
+            //return View(await Tienda.ToListAsync());
+
+
+        }
+      /*  public IActionResult Compromiso()
+        {
+            return View();
+        }
+        */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Notas")] Compromiso compromiso)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                _context.Add(compromiso);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+ 
+            return View(compromiso);
+        }
+
     }
 }
